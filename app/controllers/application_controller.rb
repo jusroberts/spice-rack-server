b849fe6e9942914a9c7d8e8761383892f3e20d42
Posey <rangemonk@gmail.com>
2014-11-12 01:27:32 -0500

    ghetto ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :enforce_ssl
+
+  private
+
+  def enforce_ssl
+    if request.url.split(':')[0] == 'http'
+      redirect_to request.url.sub('http', 'https')
+    end if Rails.env.production?
+  end
+
 end

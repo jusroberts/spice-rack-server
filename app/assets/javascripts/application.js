fd42840269dfe00c792111ef8a35173229ee2950
Posey <rangemonk@gmail.com>
2014-11-12 14:46:01 -0500

    ux stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,4 @@
 //= require foundation
 //= require_tree .
 
-if (window.location.href.indexOf('http://') === 0 && window.location.href.indexOf('.dev') < 0)
-  window.location = window.location.href.replace('http://', 'https://');
-
 $(function() { $(document).foundation() });

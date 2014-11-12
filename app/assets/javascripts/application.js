10636cf5edd3b54d3c1050f8f29c1974d7f6ef8b
Posey <rangemonk@gmail.com>
2014-11-12 01:36:05 -0500

    super ghetto ssl

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,7 @@
 //= require foundation
 //= require_tree .
 
+if (window.location.href.indexOf('http://') === 0 && window.location.href.indexOf('.dev') < 0)
+  window.location = window.location.href.replace('http://', 'https://');
+
 $(function() { $(document).foundation() });

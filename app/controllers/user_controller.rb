16d06ced812f04c36cca6f43e549622ed5525bfa
Posey <rangemonk@gmail.com>
2014-11-14 18:57:39 -0500

    some updates

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,16 +4,23 @@ class UserController < DeviseController
 
   def magic
     self.resource = resource_class.new_with_session(sign_up_params || {}, session)
+
     resource_saved = resource.save
+
     yield resource if block_given?
+
     if resource_saved
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_in(resource_name, resource)
+
+        sign_in resource_name, resource
+
         respond_with resource, location: '/'
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+
         expire_data_after_sign_in!
+
         respond_with resource, location: '/'
       end
     else
@@ -26,10 +33,14 @@ class UserController < DeviseController
       end
 
       if resource.errors.messages[:email] == [ 'has already been taken' ]
-        self.resource = warden.authenticate!(auth_options)
-        set_flash_message(:notice, :signed_in) if is_flashing_format?
-        sign_in(resource_name, resource)
+        self.resource = warden.authenticate! auth_options
+
+        set_flash_message :notice, :signed_in if is_flashing_format?
+
+        sign_in resource_name, resource
+
         yield resource if block_given?
+
         respond_with resource, location: after_sign_in_path_for(resource)
       else
         respond_with resource
@@ -44,7 +55,7 @@ class UserController < DeviseController
   private
 
   def sign_up_params
-    devise_parameter_sanitizer.sanitize(:sign_up)
+    devise_parameter_sanitizer.sanitize :sign_up
   end
 
   def auth_options
